29b45328ee5fd0f7ec1b0a3926a7db5952051198
KEG <kory.gauger@ufl.edu>
2023-09-14 23:43:01 -0400

    added mod-tap to some more keys

--- keyboards/sofle_choc/keymaps/KEG/keymap.c
+++ keyboards/sofle_choc/keymaps/KEG/keymap.c
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT(
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_MINS,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_EQL,
-  KC_LALT,   KC_A,   KC_S,   KC_D, LSFT_T(KC_F), KC_G,                   KC_H,RSFT_T(KC_J),  KC_K,    KC_L, KC_SCLN,  KC_QUOT,
+  KC_LALT,   KC_A,LALT_T(KC_S),LCTL_T(KC_D), LSFT_T(KC_F), KC_G,                   KC_H,RSFT_T(KC_J),RCTL_T(KC_K),RALT_T(KC_L), KC_SCLN,  KC_QUOT,
   KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,     KC_MPLY, KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ENT,
                  KC_LGUI,KC_F4,KC_F5,KC_SPC, LT(_NAV,KC_BSPC),  LT(_SYM,KC_SPC), TT(_NUM), KC_DEL, KC_F12, KC_F11
 ),
@@ -66,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_COLEMAK] = LAYOUT(
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_MINS,
   KC_TAB,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_G,                      KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN,  KC_EQL,
-  KC_LALT,   KC_A,   KC_R,    KC_S, LSFT_T(KC_T),    KC_D,                KC_H, RSFT_T(KC_N),KC_E,    KC_I,    KC_O,  KC_QUOT,
+  KC_LALT,  KC_A,LALT_T(KC_R),RCTL_T(KC_S),LSFT_T(KC_T),KC_D,             KC_H, RSFT_T(KC_N),RCTL_T(KC_E),RALT_T(KC_I),KC_O,  KC_QUOT,
   KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, _______,     _______,KC_K,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ENT,
                  KC_LGUI,KC_F4,KC_F1, KC_SPC, LT(_NAV,KC_BSPC),      LT(_SYM,KC_SPC),  TT(_NUM), KC_DEL, KC_F12, KC_F11
 ),
