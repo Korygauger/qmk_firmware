ca1401bacc4e5b23ba9f509f903702f7390360dd
Korygauger <kory.gauger@gmail.com>
2023-11-01 23:43:14 -0400

    added f keys to the gameh layer

--- keyboards/sofle_choc/keymaps/KEG/keymap.c
+++ keyboards/sofle_choc/keymaps/KEG/keymap.c
@@ -84,9 +84,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_GAMEH] = LAYOUT(
-  KC_ESC,   KC_1,   KC_2,     KC_3,    KC_4,   KC_5,                          KC_6,        KC_7,    KC_8,    KC_9,    KC_0,  KC_MINS,
+  KC_ESC,   KC_F1,   KC_F2,     KC_F3,    KC_F4,   KC_F5,                          KC_F6,        KC_F7,    KC_F8,    KC_F9,    KC_0,  KC_MINS,
     KC_TAB,  KC_CTLTAB,   KC_UP,     KC_ALTAB,    KC_R,   KC_T,                          KC_Y,     KC_BTN5, KC_MS_U, KC_BTN4,    KC_P,  KC_EQL,
-    KC_LCTL, KC_LEFT,   KC_DOWN, KC_RGHT,    KC_F,   KC_G,                          KC_H,     KC_MS_L, KC_MS_D, KC_MS_R, KC_BTN2,  KC_QUOT,
+    KC_LCTL, KC_LEFT,   KC_DOWN, KC_RGHT,    KC_J,   KC_G,                          KC_H,     KC_MS_L, KC_MS_D, KC_MS_R, KC_BTN2,  KC_QUOT,
     KC_LSFT, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, _______,     _______,    KC_N,        KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ENT,
                      KC_M,   KC_O, _______, KC_LALT, KC_SPC,    TG(_GAME), KC_BTN1, LALT(KC_F4),  KC_F12,  KC_F11
 ),
